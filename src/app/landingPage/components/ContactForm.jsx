4ab2bfc3302c54d873653790831ecb828a7cc745
remeessalam <remeesreme4u@gmail.com>
2025-04-22 17:08:43 +0530

    google tag added and updated the backend form submision

--- src/app/landingPage/components/ContactForm.jsx
+++ src/app/landingPage/components/ContactForm.jsx
@@ -31,18 +31,22 @@ const ContactForm = ({ setLoading }) => {
 
     // Construct the request payload
     var payload = {
+      name: companyDetails.name,
       to: companyDetails.email,
       subject: "You have a new message - Flamingo",
       body: emailBody,
     };
 
-    await fetch("https://smtp-api-tawny.vercel.app/send-email", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(payload),
-    })
+    await fetch(
+      "https://send-mail-redirect-boostmysites.vercel.app/send-email",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(payload),
+      }
+    )
       .then((response) => response.json())
       .then((res) => {
         if (res.error) {
